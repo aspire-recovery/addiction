993f65148d32412e8115911b438ea602db64b460
umang kalavadiya <umang.kalavadiya@gmail.com>
2021-02-28 12:19:35 +0530

    phychatris ui

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbname = "addiction";
 
 //session_start ();
 // Create connection
-$conn = mysqli_connect ($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 //Check connection
 if (!$conn)
 {
