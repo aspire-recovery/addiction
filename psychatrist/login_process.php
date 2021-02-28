da31889cfbd5346b63df94cc99110b8362d9e291
umang kalavadiya <umang.kalavadiya@gmail.com>
2021-02-28 13:58:37 +0530

    das

---
+++ psychatrist/login_process.php
@@ -0,0 +1,37 @@
+<?php
+// Imports
+require 'config.php';
+session_start();
+
+if (isset($_POST['submit'])) {
+//Data Fetch
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo $email, $password;
+
+//Query
+    $login_sql = "SELECT * FROM `user` WHERE u_email='$email' AND u_password'$password'";
+    echo $login_sql;
+
+    $result = $conn->query($login_sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $u_id = $row['u_id'];
+            $u_email = $row['u_email'];
+            $u_name = $row['u_name'];
+        }
+
+        $_SESSION['u_id'] = $u_id;
+        $_SESSION['u_email'] = $u_email;
+        $_SESSION['u_name'] = $u_name;
+
+        header('Location:index.php');
+    } else {
+        echo '<script>alert("Invalid Email and Password");</script>';
+        echo '<script>window.location.href="login.php";</script>';
+    }
+}
+?>
+
---
+++ psychatrist/temp.php
@@ -0,0 +1,67 @@
+<?php
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <link href="//fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Aspire Recovery</title>
+</head>
+<body>
+
+<?php
+include 'header.php';
+?>
+<section class="w3l-free-consultion">
+    <div class="container">
+        <div class="consultation-grids">
+            <div class="apply-form">
+                <h5> Free Consultation</h5>
+                <form action="#" method="post">
+                    <div class="admission-form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Full Name*" required="">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Phone Number*" required="">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Email*" required="">
+                        </div>
+                        <select class="form-control">
+                            <option>Dental Crowns</option>
+                            <option>Teeth whitening</option>
+                            <option>Others</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="Comment" class="form-control" placeholder="Message*" required=""></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary submit">Get a Free Consultation</button>
+                </form>
+            </div>
+            <div class="consultation-img">
+                <img src="assets/images/ab.jpg" class="img-fluid" alt="/">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
+class temp
+{
+
+}
