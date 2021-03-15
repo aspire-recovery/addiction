fbca673689139e09a638a65972264b85007dc397
Dhruvit07 <salatd0852@gmail.com>
2021-03-16 00:42:43 +0530

    Forgot Password
    Completed!!!
    Hey! Listen before you run change the link in the file password-reset-token.php in includes folder at line 23 according to your xampp location otherwise it won't work.

--- logout.php
+++ logout.php
@@ -3,5 +3,6 @@ session_start();
 unset($_SESSION["u_name"]);
 unset($_SESSION["u_email"]);
 unset($_SESSION["u_id"]);
+session_unset();
+session_destroy();
 echo '<script>window.location.href="login.php"</script>';
-?>
