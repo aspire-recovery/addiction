fe97599e3432e37117693c4df90cf09b16842fd5
ajay-rathod0801 <ajay.rathod0801@gmail.com>
2021-04-10 18:15:43 +0530

    Static Product added

---
+++ footer.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Aspire recovery</title>
+
+    <link href="//fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="psychatrist/assets/css/style-starter.css">
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+</head>
+<body>
+<!-- //stats -->
+<div class="w3l-footer-main">
+    <div class="w3l-sub-footer-content">
+        <!-- Footers-14 -->
+        <footer class="footer-14">
+            <div id="footers14-block">
+                <div class="container">
+                    <div class="footers20-content">
+                        <div class="d-grid grid-col-4 grids-content">
+                            <div class="column">
+                                <h4>Our Address</h4>
+                                <p>MSU Polytechnic,Pandya Bridge,Sayaji Gunj,vadodara</p>
+                            </div>
+                            <div class="column">
+                                <h4>Call Us</h4>
+                                <p>Mon - Fri 10:30 -18:00</p>
+                                <p><a href="tel:+916351301322">6351301322</a></p>
+                            </div>
+                            <div class="column">
+                                <h4>Mail Us</h4>
+                                <p><a href="mailto:info@example.com">umang.kalavadiya@gmail.com</a></p>
+                                <p><a href="mailto:no.reply@example.com">Salatd0852@gmail.com</a></p>
+                                <p><a href="mailto:no.reply@example.com">ajay.rathod0801@gmail.com</a></p>
+                            </div>
+                            <div class="column">
+                                <h4>Follow Us On</h4>
+                                <ul>
+                                    <li><a href="#facebook"><span class="fa fa-facebook"
+                                                                  aria-hidden="true"></span></a>
+                                    </li>
+                                    <li><a href="#linkedin"><span class="fa fa-linkedin"
+                                                                  aria-hidden="true"></span></a>
+                                    </li>
+                                    <li><a href="#twitter"><span class="fa fa-twitter"
+                                                                 aria-hidden="true"></span></a>
+                                    </li>
+                                    <li><a href="#google"><span class="fa fa-google-plus"
+                                                                aria-hidden="true"></span></a>
+                                    </li>
+                                    <li><a href="https://github.com/valentinos2077"><span class="fa fa-github"
+                                                                                          aria-hidden="true"></span></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- //footer 14 -->
+
+            <script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+
+            <script src="assets/js/theme-change.js"></script><!-- theme switch js (light and dark)-->
+            <script src="assets/js/owl.carousel.js"></script>
+
+            <!-- script for banner slider-->
+            <script>
+                $(document).ready(function () {
+                    $('.owl-one').owlCarousel({
+                        loop: true,
+                        dots: false,
+                        margin: 0,
+                        nav: true,
+                        responsiveClass: true,
+                        autoplay: true,
+                        autoplayTimeout: 5000,
+                        autoplaySpeed: 1000,
+                        autoplayHoverPause: false,
+                        responsive: {
+                            0: {
+                                items: 1
+                            },
+                            480: {
+                                items: 1
+                            },
+                            667: {
+                                items: 1
+                            },
+                            1000: {
+                                items: 1
+                            }
+                        }
+                    })
+                })
+            </script>
+            <!-- //script -->
+
+            <!-- script for tesimonials carousel slider -->
+            <script>
+                $(document).ready(function () {
+                    $("#owl-demo1").owlCarousel({
+                        loop: true,
+                        margin: 20,
+                        nav: false,
+                        responsiveClass: true,
+                        responsive: {
+                            0: {
+                                items: 1
+                            },
+                            736: {
+                                items: 1
+                            },
+                            1000: {
+                                items: 2,
+                                loop: false
+                            }
+                        }
+                    })
+                })
+            </script>
+            <!-- //script for tesimonials carousel slider -->
+
+            <script src="assets/js/counter.js"></script>
+
+            <!--/MENU-JS-->
+            <script>
+                $(window).on("scroll", function () {
+                    var scroll = $(window).scrollTop();
+
+                    if (scroll >= 80) {
+                        $("#site-header").addClass("nav-fixed");
+                    } else {
+                        $("#site-header").removeClass("nav-fixed");
+                    }
+                });
+
+                //Main navigation Active Class Add Remove
+                $(".navbar-toggler").on("click", function () {
+                    $("header").toggleClass("active");
+                });
+                $(document).on("ready", function () {
+                    if ($(window).width() > 991) {
+                        $("header").removeClass("active");
+                    }
+                    $(window).on("resize", function () {
+                        if ($(window).width() > 991) {
+                            $("header").removeClass("active");
+                        }
+                    });
+                });
+            </script>
+            <!--//MENU-JS-->
+
+            <!-- disable body scroll which navbar is in active -->
+            <script>
+                $(function () {
+                    $('.navbar-toggler').click(function () {
+                        $('body').toggleClass('noscroll');
+                    })
+                });
+
+            </script>
+            <!-- //disable body scroll which navbar is in active -->
+
+            <!--bootstrap-->
+            <script src="assets/js/bootstrap.min.js"></script>
+            <!-- //bootstrap-->
+</body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,225 @@
+<?php
+//Imports
+require 'config.php';
+include "header.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Aspire recovery</title>
+
+    <link href="//fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="psychatrist/assets/css/style-starter.css">
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <!--    <style>-->
+    <!--        /* Bootstrap for CSS */-->
+    <!---->
+    <!--        @import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css";-->
+    <!---->
+    <!--        /*Universal styling for all cards */-->
+    <!--        body {-->
+    <!--            background: #000;-->
+    <!--        }-->
+    <!---->
+    <!--        .container {-->
+    <!--            margin-top: 110px !important;-->
+    <!--        }-->
+    <!---->
+    <!--        .card {-->
+    <!--            border: none;-->
+    <!--            outline: none;-->
+    <!--            background-color: #fff;-->
+    <!--            border-radius: 20px;-->
+    <!--            transition: transform .3s;-->
+    <!--        }-->
+    <!---->
+    <!--        .card:hover {-->
+    <!--            transform: translateY(-15px);-->
+    <!--            transition: transform .3s;-->
+    <!--        }-->
+    <!---->
+    <!--        .text1 {-->
+    <!--            font-size: 13px;-->
+    <!--            color: #cbcbcb;-->
+    <!--        }-->
+    <!---->
+    <!--        .info {-->
+    <!--            line-height: 17px;-->
+    <!--        }-->
+    <!---->
+    <!--        .star {-->
+    <!--            color: #fbc02d;-->
+    <!--        }-->
+    <!---->
+    <!--        /* card 1 */-->
+    <!--        .productcost1 span {-->
+    <!--            color: #fb3531;-->
+    <!--            font-weight: bold;-->
+    <!--            font-size: 20px;-->
+    <!--        }-->
+    <!---->
+    <!--        .pro-1 {-->
+    <!--            background: #dc3545;-->
+    <!--        }-->
+    <!---->
+    <!--        /* card 2 */-->
+    <!--        .productcost2 span {-->
+    <!--            color: #206783;-->
+    <!--            font-weight: bold;-->
+    <!--            font-size: 20px;-->
+    <!--        }-->
+    <!---->
+    <!--        .pro-2 {-->
+    <!--            background: #206783;-->
+    <!--        }-->
+    <!---->
+    <!--        /* card 3 */-->
+    <!--        .productcost3 span {-->
+    <!--            color: #0012b2;-->
+    <!--            font-weight: bold;-->
+    <!--            font-size: 20px;-->
+    <!--        }-->
+    <!---->
+    <!--        .pro-3 {-->
+    <!--            background: #0012b2;-->
+    <!--        }-->
+    <!---->
+    <!--        /*Universal styling for all product cards */-->
+    <!--        .pro-1:hover, .pro-2:hover, .pro-3:hover {-->
+    <!--            opacity: .8;-->
+    <!--        }-->
+    <!---->
+    <!--        .cursor {-->
+    <!--            cursor: pointer;-->
+    <!--            border-bottom-left-radius: 20px;-->
+    <!--            border-bottom-right-radius: 20px;-->
+    <!--        }-->
+    <!--    </style>-->
+</head>
+<body>
+<section class="w3-services-ab py-5" id="mission" style="margin-top: 55px">
+    <div class="container py-lg-5 py-md-4">
+        <h3 class="title-big text-center mb-5">Recovery Aids</h3>
+        <div class="w3-services-grids">
+            <div class="fea-gd-vv row">
+                <div class="container mb-5 mt-5">
+                    <div class="row">
+                        <!--Card 1-->
+                        <div class="col-md-4">
+                            <div class="card mt-3">
+                                <div class="product-1 align-items-center p-2 text-center">
+                                    <img src="images/image01.jpg" alt="" class="rounded" width="160">
+                                    <h5>Product 1</h5>
+
+                                    <!--Card info-->
+                                    <div class="mt-3 info">
+                                        <span class="text1 d-block">Product description ( TBD ).</span>
+                                        <span class="text1">Product description line-2</span>
+                                    </div>
+                                    <div class="productcost1 mt-3 text-dark">
+                                        <span>$69.99</span>
+                                        <div class="star mt-3 align-items-center">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                        </div>
+                                        <a href="cart.php?p_id='..'" class="btn btn-primary btn-lg" style="margin-top: 105px;">Add To
+                                            Cart</a>
+                                    </div>
+                                </div>
+
+                                <!--Button for cards-->
+                                <div class="p-3 pro-1 text-center text-white mt-3 cursor">
+                                    <span class="text-uppercase">Add to cart</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--card 1 ends here-->
+
+                        <!--Card 2-->
+                        <div class="col-md-4">
+                            <div class="card mt-3">
+                                <div class="product-1 align-items-center p-2 text-center">
+                                    <img src="" alt="" class="rounded" width="160">
+                                    <h5>Product 2</h5>
+
+                                    <!--Card info-->
+                                    <div class="mt-3 info">
+                                        <span class="text1 d-block">Product description ( TBD ).</span>
+                                        <span class="text1">Product description line-2</span>
+                                    </div>
+                                    <div class="productcost2 mt-3 text-dark">
+                                        <span>$69.99</span>
+                                        <div class="star mt-3 align-items-center">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                        </div>
+                                        <a href="cart.php?p_id='..'" class="btn btn-primary btn-lg" style="margin-top: 105px;">Add To
+                                            Cart</a>
+                                    </div>
+                                </div>
+
+                                <!--Button for cards-->
+                                <div class="p-3 pro-2 text-center text-white mt-3 cursor">
+                                    <span class="text-uppercase">Add to cart</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--card 2 ends here-->
+
+                        <!--Card 3-->
+                        <div class="col-md-4">
+                            <div class="card mt-3">
+                                <div class="product-1 align-items-center p-2 text-center">
+                                    <img src="images/image01.jpg" alt="" class="rounded" width="160">
+                                    <h5>Product 3</h5>
+
+                                    <!--Card info-->
+                                    <div class="mt-3 info">
+                                        <span class="text1 d-block">Product description ( TBD ).</span>
+                                        <span class="text1">Product description line-2</span>
+                                    </div>
+                                    <div class="productcost3 mt-3 text-dark">
+                                        <span>$69.99</span>
+                                        <div class="star mt-3 align-items-center">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                        </div>
+                                        <a href="cart.php?p_id='..'" class="btn btn-primary btn-lg" style="margin-top: 105px;">Add To
+                                            Cart</a>
+                                    </div>
+                                </div>
+
+                                <!--Button for cards-->
+                                <div class="p-3 pro-3 text-center text-white mt-3 cursor">
+                                    <span class="text-uppercase">Add to cart</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--card 3 ends here-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
