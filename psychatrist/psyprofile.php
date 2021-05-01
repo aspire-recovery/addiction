246e6f8932b350056765fb13d2e30d567e1678fb
umangkalavadiya <67776357+umangkalavadiya@users.noreply.github.com>
2021-05-01 20:22:30 +0530

    its almost done

--- psychatrist/psyprofile.php
+++ psychatrist/psyprofile.php
@@ -241,13 +241,6 @@ $image = $row['psy_profile'];
                             </a>
                         </li>
                     </ul>
-                    <div class="copyright">
-                        ©
-                        <script>
-                        document.write(new Date().getFullYear())
-                        </script>2018 made with <i class="tim-icons icon-heart-2"></i> by
-                        <a href="javascript:void(0)" target="_blank">Creative Tim</a> for a better web.
-                    </div>
                 </div>
             </footer>
         </div>
